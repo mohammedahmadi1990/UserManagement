72aea6f80c0077984b4a583a5483416a769df929
Med <mohammed.ahmadi1990@gmail.com>
2022-05-30 19:23:58 +0300

    FirstCommit
    SpringBoot initialized with:
    Web Starter
    Lombok
    JPA
    Web Security
    Liquibase
    Eureka Service Discovery Client
    MySQL Driver
    Rest Repository

---
+++ src/test/java/me/designdevelop/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package me.designdevelop.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
